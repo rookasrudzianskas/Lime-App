224c9010122e84ab83ca3baaf257c18ba330362b
rokas <rokas@rokas.com>
2024-06-27 14:26:17 +0300

    fix

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import Mapbox, {
   Camera,
   LocationPuck,
@@ -6,23 +7,43 @@ import Mapbox, {
   SymbolLayer,
   Images,
   CircleLayer,
+  LineLayer,
 } from '@rnmapbox/maps';
 import { featureCollection, point } from '@turf/helpers';
-
-// @ts-ignore
+import * as Location from 'expo-location';
+import { useState } from 'react';
+import { OnPressEvent } from '@rnmapbox/maps/lib/typescript/src/types/OnPressEvent';
 import pin from '~/assets/pin.png';
 import scooters from '~/data/scooters.json';
+import { getDirections } from "~/directions";
 
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || '');
 
+
+/**
+ * Renders a map view with markers for scooters and a route if one is selected.
+ *
+ * @return {JSX.Element} The rendered map view.
+ */
 export default function Map() {
+  const [direction, setDirection] = useState();
   const points = scooters.map((scooter) => point([scooter.long, scooter.lat]));
+  const directionCoordinate = direction?.routes?.[0]?.geometry.coordinates;
+
+  const onPointPress = async (event: OnPressEvent) => {
+    const myLocation = await Location.getCurrentPositionAsync();
+    const newDirection = await getDirections(
+      [myLocation.coords.longitude, myLocation.coords.latitude],
+      [event.coordinates.longitude, event.coordinates.latitude]
+    );
+    setDirection(newDirection);
+  };
 
   return (
     <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
-      <Camera followZoomLevel={10} followUserLocation />
+      <Camera followZoomLevel={14} followUserLocation />
       <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
-      <ShapeSource id="scooters" shape={featureCollection(points)}>
+      <ShapeSource id="scooters" shape={featureCollection(points)} onPress={onPointPress}>
         <CircleLayer
           id="clusters"
           belowLayerID="clusters-count"
@@ -48,6 +69,32 @@ export default function Map() {
         />
         <Images images={{ pin }} />
       </ShapeSource>
+
+      {directionCoordinate && (
+        <ShapeSource
+          id="routeSource"
+          lineMetrics
+          shape={{
+            properties: {},
+            type: 'Feature',
+            geometry: {
+              type: 'LineString',
+              coordinates: directionCoordinate,
+            },
+          }}>
+          <LineLayer
+            id="exampleLineLayer"
+            style={{
+              lineColor: '#42E100',
+              lineCap: 'round',
+              lineJoin: 'round',
+              lineWidth: 7,
+            }}
+          />
+
+        </ShapeSource>
+      )}
+
     </MapView>
   );
 }
